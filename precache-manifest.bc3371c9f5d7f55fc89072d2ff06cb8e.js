dba3f3e772d74e93eaef9241c8f8d3f8cd40794c
Aliaksandr Rudyk <dezo103@gmail.com>
2022-02-01 12:20:54 +0300

    Updates

---
+++ precache-manifest.bc3371c9f5d7f55fc89072d2ff06cb8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cf2f2b586903e5b82723f5859f714c6",
+    "url": "/HW/index.html"
+  },
+  {
+    "revision": "74f903c5fd40e073856f",
+    "url": "/HW/static/css/main.066b8067.chunk.css"
+  },
+  {
+    "revision": "408c3bd4ef333d9675b5",
+    "url": "/HW/static/js/2.8bf86235.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW/static/js/2.8bf86235.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74f903c5fd40e073856f",
+    "url": "/HW/static/js/main.64df5775.chunk.js"
+  },
+  {
+    "revision": "966498bdce7dc324dc93",
+    "url": "/HW/static/js/runtime-main.c19bd276.js"
+  }
+]);
