d351826ba575afd23d2329ba1a989378b96b6024
Aliaksandr Rudyk <dezo103@gmail.com>
2022-02-01 18:04:38 +0300

    add HW 5

--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Header from './Header'
 import AllRoutes from './Routes'
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 function HW5() {
     return (
         <div>
             {/*в gh-pages лучше работает HashRouter*/}
             {/*<HashRouter>*/}
-            <BrowserRouter>
+            <HashRouter>
                 <Header/>
 
                 <AllRoutes/>
-            </BrowserRouter>
+            </HashRouter>
             {/*</HashRouter>*/}
         </div>
     )
--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import {PATH} from "./Routes";
+import {NavLink} from 'react-router-dom'
+import s from "./Header.module.css"
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <div className={s.nav_item}><NavLink to={PATH.PRE_JUNIOR}>Pre-Junior</NavLink></div>
+            <div className={s.nav_item}><NavLink to={PATH.JUNIOR}>Junior</NavLink></div>
+            <div className={s.nav_item}><NavLink to={PATH.JUNIOR_PLUS}>Junior-Plus</NavLink></div>
         </div>
     )
 }
